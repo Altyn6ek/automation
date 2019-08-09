1651653161fa4bc87ab5530707a6603f03710259
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-08-09 17:48:19 +0600

    end of the 2_2 lesson

---
+++ application.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+
+
+class Application:
+
+    def __init__(self):
+        self.driver = webdriver.Firefox(executable_path=r'D:\python for testers\Python for testers\учебное '
+                                                        r'приложение\geckodriver.exe')
+        self.driver.implicitly_wait(30)
+
+    def open_home_page(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        driver = self.driver
+        # login
+        self.open_home_page()
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_id("LoginForm").submit()
+
+    def open_groups_page(self):
+        driver = self.driver
+        driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, group):
+        driver = self.driver
+        self.open_groups_page()
+        # init group creation
+        driver.find_element_by_name("new").click()
+        # fill group firm
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        driver.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        driver = self.driver
+        driver.find_element_by_link_text("group page").click()
+
+    def logout(self):
+        driver = self.driver
+        driver.find_element_by_link_text("Logout").click()
+
+    def destroy(self):
+        self.driver.quit()
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,109 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class AppDynamicsJob(unittest.TestCase):
-    def setUp(self):
-        # AppDynamics will automatically override this web driver
-        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
-        self.driver = webdriver.Firefox(executable_path=r'D:\python for testers\Python for testers\учебное '
-                                                        r'приложение\geckodriver.exe')
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def test_add_group(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
-        self.create_group(driver, Group(name="group_name", header="group_header", footer="group_footer"))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
-
-    def test_add_empty_group(self):
-        driver = self.driver
-        self.open_home_page(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
-        self.create_group(driver, Group(name="", header="", footer=""))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
-
-    def logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, driver):
-        driver.find_element_by_link_text("group page").click()
-
-    def create_group(self, driver, group):
-        # init group creation
-        driver.find_element_by_name("new").click()
-        # fill group firm
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        driver.find_element_by_name("submit").click()
-
-    def open_groups_page(self, driver):
-        driver.find_element_by_link_text("groups").click()
-
-    def login(self, driver, username, password):
-        # login
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_id("LoginForm").submit()
-
-    def open_home_page(self, driver):
-        driver.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        # To know more about the difference between verify and assert,
-        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
-        self.assertEqual([], self.verificationErrors)
-        self.driver.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy())
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group_name", header="group_header", footer="group_footer"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
