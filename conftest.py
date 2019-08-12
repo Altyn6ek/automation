d707393c1eec3c6af00a957e43973f067ba408d5
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-08-12 18:32:33 +0600

    end of the 2_4_decomposition lesson, working with session, added requirements.txt

--- conftest.py
+++ conftest.py
@@ -2,9 +2,15 @@ from fixture.application import Application
 import pytest
 
 
-@pytest.fixture(scope="session")
-# @pytest.fixture()
+# @pytest.fixture(scope='session')
+@pytest.fixture(scope='session')
 def app():
     fixture = Application()
     yield fixture
     fixture.destroy()
+
+# @pytest.fixture(scope='session')
+# def app(request):
+#     fixture = Application()
+#     request.addfinalizer(fixture.destroy)
+#     return fixture
--- fixture/application.py
+++ fixture/application.py
@@ -18,4 +18,3 @@ class Application:
 
     def destroy(self):
         self.driver.quit()
-
