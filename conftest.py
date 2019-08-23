b329d89061903546eabcb34cadce0e92740b7f36
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-08-23 18:22:55 +0600

    end of the 3_1_test_for_modification lesson, added 2 tests with modifying groups. Created new additional methods in GroupHelper

--- conftest.py
+++ conftest.py
@@ -2,15 +2,8 @@ from fixture.application import Application
 import pytest
 
 
-# @pytest.fixture(scope='session')
 @pytest.fixture()
 def app():
     fixture = Application()
     yield fixture
     fixture.destroy()
-
-# @pytest.fixture(scope='session')
-# def app(request):
-#     fixture = Application()
-#     request.addfinalizer(fixture.destroy)
-#     return fixture
