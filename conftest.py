3cba4821c0ab3ec88702b7f5dbd2773659fc54e0
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-08-26 13:49:18 +0600

    end of the 3_2_fixtures

--- conftest.py
+++ conftest.py
@@ -2,8 +2,26 @@ from fixture.application import Application
 import pytest
 
 
+fixture = None
+
+
 @pytest.fixture()
 def app():
-    fixture = Application()
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
     yield fixture
-    fixture.destroy()
+    request.addfinalizer(fin)
