80205b0a59ddc127517cf812fafc970e2545937b
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-08-27 11:35:17 +0600

    end of the 3_3_login.mp4, works without ensure_logout()

--- fixture/session.py
+++ fixture/session.py
@@ -17,17 +17,12 @@ class SessionHelper:
         driver = self.app.driver
         driver.find_element_by_link_text("Logout").click()
 
-    def ensure_logout(self):
-        driver = self.app.driver
-        if self.is_logged_in():
-            self.logout()
-
     def is_logged_in(self):
         driver = self.app.driver
-        return len(driver.find_elements_link_text("Logout")) > 0
+        return len(driver.find_elements_by_link_text("Logout")) > 0
 
     def ensure_login(self, username, password):
-        driver = self.app.driver
+        # driver = self.app.driver
         if self.is_logged_in():
             if self.is_logged_in_as(username):
                 return
