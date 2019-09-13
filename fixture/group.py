f998041eb05de24a368281c8de22b7c9ed5cb32f
Altynbek_Topayev <altynbek_topayev@gmail.com>
2019-09-13 15:56:57 +0600

    end of the 4_4_optimization.mp4

--- fixture/group.py
+++ fixture/group.py
@@ -20,6 +20,7 @@ class GroupHelper:
         # submit group creation
         driver.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def change_field_value(self, field_name, text):
         driver = self.app.driver
@@ -45,6 +46,7 @@ class GroupHelper:
         # submit deletion
         driver.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def modify_first_group(self, new_group_data):
         driver = self.app.driver
@@ -57,6 +59,7 @@ class GroupHelper:
         # submit modification
         driver.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def return_to_groups_page(self):
         driver = self.app.driver
@@ -67,12 +70,15 @@ class GroupHelper:
         self.open_groups_page()
         return len(driver.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        driver = self.app.driver
-        self.open_groups_page()
-        groups = []
-        for element in driver.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            driver = self.app.driver
+            self.open_groups_page()
+            self.group_cache = []
+            for element in driver.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
